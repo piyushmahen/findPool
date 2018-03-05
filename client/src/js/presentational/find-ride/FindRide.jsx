bb8e9de0a4f409a8433ad7364a6d7b3efdd254ed
piyush-ck12 <24309737+piyushmahen@users.noreply.github.com>
2018-03-06 04:22:08 +0530

    added key

--- client/src/js/presentational/find-ride/FindRide.jsx
+++ client/src/js/presentational/find-ride/FindRide.jsx
@@ -190,7 +190,7 @@ class FindRide extends Component {
               return (
                 <MyMapComponent
                   isMarkerShown
-                  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC-cVSGLPxEm48U9APAiq1tO9wpcgcDkZE&v=3.exp&libraries=geometry,drawing,places"
                   loadingElement={<div style={{ height: `100%` }} />}
                   containerElement={<div style={{ height: `350px` }} />}
                   mapElement={<div style={{ height: `100%` }} />}
